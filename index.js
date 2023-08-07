69bda7b5925a4fcb1707872b260db8b2ab8717f7
unknown <barronpeterson56@gmail.com>
2023-08-07 12:02:20 -0500

    add comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+//get all of the dependencies that are needed
 const fs = require('fs');
 const inquire = require('inquirer');
-const shapes = require('./lib/shapes');
 const {characterValidate,colorValidate} = require('./lib/validate.js');
 const generateSVG = require('./lib/generateSVG.js');
 
-
+//creates all the questions that will be asked of the user
 const questions = [
     {
         type:'input',
@@ -36,18 +36,18 @@ const questions = [
         validate: colorValidate
     }
 ]
-
+//after the questions have been asked this function writes the file to the examples folder
 function writeSVG(fileName,data){
 fs.writeFile(fileName,generateSVG(data),(err)=>{
     if(err) throw err;
 })
 }
 
-
+//initialize function it prompts the questions and then passes the data to the writeSVG function
 function init(){
     inquire.prompt(questions)
         .then((data)=>{writeSVG(`./examples/${data.title}.svg`,data)})
 }
 
-
+// calls the init function
 init()
